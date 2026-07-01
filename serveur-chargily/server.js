12788af866299ec9d3dfeab7e79d6b055d38ad02
tintin <tinhinane.belarbi@se.univ-bejaia.dz>
2026-07-01 22:38:15 +0100

    server.js

--- serveur-chargily/server.js
+++ serveur-chargily/server.js
@@ -27,6 +27,7 @@ let db;
 async function connectDB() {
   db = await mysql.createConnection({
     host:     process.env.DB_HOST     || "localhost",
+    port:     process.env.DB_PORT     || 3306,
     user:     process.env.DB_USER     || "root",
     password: process.env.DB_PASSWORD || "",
     database: process.env.DB_NAME     || "autoecole",
